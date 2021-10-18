211ca54edcbc9656d7e915c38192dd1d24670159
Mario Bruno <mbrunostem@gmail.com>
2021-10-18 13:00:03 -0400

    new html elements, flex css

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var cityInputEl = document.querySelector('#city-input');
+var searchBtnEl = document.querySelector('#search-button');
+var searchHistoryEl = document.querySelector('#search-history');
+var searchHistoryEl = document.querySelector('#current-day');
+var forecastEl = document.querySelector('#forecast');
+var city = "";
+var APIKey = '8106e0bea7fc031672ade780d6c17d4d';
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+localStorage.setItem("recent-search", JSON.stringify(city))
+
+function fetchWeather(){
+    // var city = cityInputEl.value;
+    // var APIKey = '8106e0bea7fc031672ade780d6c17d4d'
+    // var queryURL = "http://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + APIKey;
+    fetch(queryURL)
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+searchBtnEl.addEventListener('click', fetchWeather);
